372483dc7e55d9e3bfa3dc87f657e2cd174ea0f7
unknown <junaidarshad113@gmail.com>
2022-10-05 00:57:22 -0700

    Updates

---
+++ precache-manifest.9fdf0d7bde92eb7387fc7684d1758f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ecfe2f05c1b9f697fd08f19585d572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631347fa4946bc3925a0",
+    "url": "/static/css/main.690a90d5.chunk.css"
+  },
+  {
+    "revision": "1c6fcaf4727815fbe36e",
+    "url": "/static/js/2.a1a93356.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.a1a93356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631347fa4946bc3925a0",
+    "url": "/static/js/main.6cfd420b.chunk.js"
+  },
+  {
+    "revision": "7f98fa0c0d2b6ecdf360",
+    "url": "/static/js/runtime-main.be092252.js"
+  }
+]);
